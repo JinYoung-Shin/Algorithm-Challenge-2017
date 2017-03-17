88b4cd5dfc254758d9dd143be776dbad3167bb10
hangil-jung <hangil.jung@goorm.io>
2017-03-17 01:42:28 +0000

    test.sh가 여러개의 input.txt에도 도 동작하도록 변경

--- hg/test.sh
+++ hg/test.sh
@@ -2,4 +2,20 @@
 
 cd $1
 g++ src/$2.cpp -o bin/$2.out
-bin/$2.out < input.txt
+
+COMPILER_RET_STATUS=$?
+if [ "$COMPILER_RET_STATUS" != "0" ]
+then
+	echo "컴파일 에러!"
+	exit 1
+fi
+
+INPUT_ARR=($(ls input*.txt))
+
+for var in "${INPUT_ARR[@]}"
+do
+	echo "${var}: 입력 케이스 ---------"
+	bin/$2.out < ${var}
+	echo "----------------------------------------"
+	echo ""
+done

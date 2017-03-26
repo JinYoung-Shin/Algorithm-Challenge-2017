263f6c42f31d3b8aaf2cf556bf96262187959a62
JinYoung-Shin <wlsdk0710@naver.com>
2017-03-26 15:53:45 +0000

    SCPC_42_coredump

---
+++ JYS/SCPC/42_subarray.cpp
@@ -0,0 +1,38 @@
+#include <cstdio>
+#include <algorithm>
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	
+	int T, N, S;
+	int l = 0;
+	int arr[100001] = {0};
+	int sum[100001] = {0};
+	
+	scanf("%d", &T);
+
+	for( int i = 0; i < T; i++ ) {
+		scanf("%d %d", &N, &S);
+		for( int j = 0; j < N; j++ ) {
+			cin >> arr[j];
+		}
+		
+		printf("#testcase%d\n", i+1);
+		
+		// Compare if S <
+		while(1) {
+			for( int k = 0; k < N; k ++ ) {
+				sum[k] += arr[k+l];
+				if(sum[k] > S) {
+					printf("%d", k+1);
+					break;
+				}
+			}
+			l++;
+		}
+	}
+	
+	return 0;
+}

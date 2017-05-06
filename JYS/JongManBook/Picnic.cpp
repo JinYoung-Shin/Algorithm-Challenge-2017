57e33e042217302894befa2d571fafef920815ee
JinYoung-Shin <wlsdk0710@naver.com>
2017-05-06 16:34:52 +0900

    Update Picnic.cpp

--- JYS/JongManBook/Picnic.cpp
+++ JYS/JongManBook/Picnic.cpp
@@ -5,6 +5,59 @@
 //  Created by Spiritsensor on 2017. 3. 9..
 //  Copyright © 2017년 JYS. All rights reserved.
 //
+#include <iostream>
+#include <cstdio>
+#include <algorithm>
+
+using namespace std;
+
+int T;
+int n, m;
+bool friends[15][15] = {false};
+bool done[15] = {false};
+
+int f1, f2;
+int firstFree;
+
+int paring(bool done[15]) {
+	firstFree = -1;
+	for(int i = 0;  i < n; i++) {
+		if(!done[i]) {
+			firstFree = i;
+			break;
+		}
+	}
+	
+	if(firstFree == -1) {
+		return 1;
+	}
+	int ans = 0;
+	// pair with next friends
+	for(int i = firstFree+1; i < n; i++) {
+		if(friends[firstFree][i] && !done[i]) {
+			done[firstFree] = done[i] = true;
+			ans += paring(done);
+			done[firstFree] = done[i] = false;
+		}
+	}
+	return ans;
+}
+
+int main() {
+	scanf("%d", &T);
+	for(int t = 1; t <= T; t++) {
+		scanf("%d %d", &n, &m);
+		for(int i = 0; i < m; i++) {
+			scanf("%d %d", &f1, &f2);
+			friends[f1][f2] = friends[f2][f1] = true;
+		}
+	}
+	// print available pair
+	printf("%d", paring(done));
+	return 0;
+}
+
+/*
 #include <cstdio>
 #include <iostream>
 #include <vector>
@@ -55,3 +108,4 @@ int main(int argc, const char * argv[]) {
     }
     return 0;
 }
+*/

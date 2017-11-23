87f35f8dea6d7f5156845dc61dbc54f1e11ba6b7
Bobgil <hg9587@gmail.com>
2017-11-23 23:34:34 +0000

    max heap, min heap

--- hg/baekjoon_lecture/etc/1927-min-heap/src/main.cpp
+++ hg/baekjoon_lecture/etc/1927-min-heap/src/main.cpp
@@ -4,15 +4,15 @@
 using namespace std;
 #define MAX_SIZE 100001
 
-class max_heap {
+class min_heap {
 	private:
 	vector<int> data;
 	public:
 	
-	max_heap() {
+	min_heap() {
 		this->data.push_back(-1); // index 0 means dummy data
 	}
-	max_heap(int size) {
+	min_heap(int size) {
 		this->data.reserve(size);
 		this->data.push_back(-1); //index 0 is dummy data
 	}
@@ -30,15 +30,15 @@ class max_heap {
 		swap(this->data[current], this->data[last]);
 		this->data.pop_back();
 		while (this->data.size() > left_child) { //when left child exist
-			int larger_index = left_child;
+			int smaller_index = left_child;
 			if (this->data.size() > right_child) { //when right child exist
-				if (this->data[right_child] > this->data[left_child]) {
-					larger_index = right_child;
+				if (this->data[right_child] < this->data[left_child]) {
+					smaller_index = right_child;
 				}
 			}
-			if (this->data[larger_index] > this->data[current]) {
-				swap(this->data[current], this->data[larger_index]);
-				current = larger_index;
+			if (this->data[smaller_index] < this->data[current]) {
+				swap(this->data[current], this->data[smaller_index]);
+				current = smaller_index;
 				left_child = current * 2;
 				right_child = left_child + 1;
 			} else {
@@ -52,7 +52,7 @@ class max_heap {
 		this->data.push_back(val);
 		int current = data.size() - 1;
 		int parent = current/2;
-		while (parent > 0 && this->data[current] > this->data[parent]) {
+		while (parent > 0 && this->data[current] < this->data[parent]) {
 			swap(this->data[current], this->data[parent]);
 			current = parent;
 			parent = current/2;
@@ -62,7 +62,7 @@ class max_heap {
 
 
 int main() {
-	max_heap heap(MAX_SIZE);
+	min_heap heap(MAX_SIZE);
 	int n;
 	cin >> n;
 	for (int i=0; i < n; i++) {
